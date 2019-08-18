21e70bfd62d459dc0e599ac834b889dfde574f64
zhongjidalao <mdd12138@163.com>
2019-08-18 18:17:57 +0800

    完善说明

--- ins_spider.py
+++ ins_spider.py
@@ -1,7 +1,7 @@
 import requests
 import re
 
-response = requests.get(input('请输入要下载的ins视频网址：'))
+response = requests.get(input('请输入要下载的ins图片或视频网址：'))
 
 form = re.findall(r'<meta name="medium" content="(.*?)" />', response.text, re.S)[0]
 
